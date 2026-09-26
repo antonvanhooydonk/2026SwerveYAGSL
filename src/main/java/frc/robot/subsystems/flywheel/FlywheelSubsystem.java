4f000f8823585241cacf60832f108862b53ada09
Anton vanHooydonk <antonvanhooydonk@gmail.com>
2026-09-25 20:47:39 -0400

    asdf

--- src/main/java/frc/robot/subsystems/flywheel/FlywheelSubsystem.java
+++ src/main/java/frc/robot/subsystems/flywheel/FlywheelSubsystem.java
@@ -30,7 +30,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 import frc.robot.Constants.CANConstants;
-import frc.robot.subsystems.intake.IntakeConstants;
 import frc.robot.util.Utils;
 
 /**
